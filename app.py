e50c1637b5c9f6bcd9db0b1553570a1e44e6daa9
Shara Rahman Khan <sharakhan646@gmail.com>
2026-06-16 01:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 mood_quotes = {
     "happy": "Keep shining. Your energy can light up someone's day ✨",
